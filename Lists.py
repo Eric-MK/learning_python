d1b16bb0ae144b615688e76747e4d15a14330e21
unknown <mutungaeric839@gmail.com>
2023-09-02 12:37:18 +0300

    Lists copy and reference

--- Lists.py
+++ Lists.py
@@ -2,6 +2,8 @@
 #can make a list within the elements of the list
 
 a = [4, True, "hi"]
-a.pop()# removes an element from the list 
+y = a #y becomes a reference of list a
+y = a[:] #y becomes a copy of a 
+a[0] = "heyyy"
 print (a)
 print(len(a)) #print lenght of the list
