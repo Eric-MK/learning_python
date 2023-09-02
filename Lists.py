9df1aebeda2f7aafecf3b69c97fc4a92e2d86b9a
unknown <mutungaeric839@gmail.com>
2023-09-02 12:32:19 +0300

    Lists extend function

--- Lists.py
+++ Lists.py
@@ -2,6 +2,8 @@
 #can make a list within the elements of the list
 
 a = [4, True, "hi"]
-a.append("wassup")# add to the end of the list
+a.extend(["a",8,5,6,7,9])# add another list to the end of it to become one list
+""" a.append("wassup")# add to the end of the list
+ """
 print (a)
 print(len(a)) #print lenght of the list
