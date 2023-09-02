711618ee0647b7b1c33c9f7d98689981dab59907
unknown <mutungaeric839@gmail.com>
2023-09-02 14:37:11 +0300

    combining lists to get a dictonary

--- dictonary.py
+++ dictonary.py
@@ -1,6 +1,14 @@
-data = {1:'Navin',2:'Kiran', 4:'Harsh'}
+""" data = {1:'Navin',2:'Kiran', 4:'Harsh'}
 
 print(data)
 print(data[4])
 print(data.get(1))
-print(data.get(3))
+print(data.get(3)) """
+
+keys = ['Navin','Kiran','Harsh']
+
+values = ['Python', 'Java','JS']
+
+data = dict(zip(keys,values)) # combine the two lists and convert to a dict
+
+print(data)
